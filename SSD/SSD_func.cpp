dc41276f941df1df69493ee23c9ee4e70561f0bf
HUISEONG-CHO <huisleee@gmail.com>
2025-05-16 16:43:06 +0900

    [FEAT] Complete InitWrite Function (TDD : Green)

--- SSD/SSD_func.cpp
+++ SSD/SSD_func.cpp
@@ -1,10 +1,21 @@
 #include "SSD_func.h"
 
-void SSD::write(int LBA, int Val) {
+void SSD::write(unsigned int LBA, unsigned int Val) {
     return;
 }
 
 void SSD::initwrite() {
+    std::ofstream ofs("ssd_nand.txt");
+    if (!ofs.is_open()) return;
+
+    for (int i = 0; i < 100; ++i) {
+        std::ostringstream line;
+        line << std::setw(2) << std::setfill('0') << i;
+        line << " 0x" << std::setw(8) << std::setfill('0') << std::hex << std::uppercase << 0;
+        ofs << line.str() << "\n";
+    }
+
+    ofs.close();
     return;
 }
 
--- SSD/SSD_func.h
+++ SSD/SSD_func.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <sstream>
 
 class SSD {
 public:
-    void write(int LBA, int Val);
+    void write(unsigned int LBA, unsigned int Val);
     void initwrite();
     int read(int LBA);
 };
