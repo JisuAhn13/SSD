1617b52b6576e4db9481b691fc86a8fb0af161cc
miadevgithub <mia.publ@gmail.com>
2025-05-19 15:13:27 +0900

    [REFAC] Change argument read condition for write operation

--- SSD/CommandChecker.cpp
+++ SSD/CommandChecker.cpp
@@ -8,7 +8,7 @@ bool CommandChecker::execute(int argc, char* argv[])
 	std::string op = std::string(argv[1]);
 	std::string lba = std::string(argv[2]);
 	std::string value;
-	if (argc >= 4) {
+	if (op == "W") {
 		value = std::string(argv[3]);
 	}
 	const std::string filename = "ssd_output.txt";
