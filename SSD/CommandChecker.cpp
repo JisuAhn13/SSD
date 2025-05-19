8f6ab402908ca181e0a0e8b70b0d46a4e0e1c8ed
miadevgithub <mia.publ@gmail.com>
2025-05-19 14:49:41 +0900

    [FIX] Check valid address when write operation

--- SSD/CommandChecker.cpp
+++ SSD/CommandChecker.cpp
@@ -30,13 +30,16 @@ bool CommandChecker::execute(int argc, char* argv[])
 		return false;
 	}
 
-	if (isValidAddress(value) == false) {
-		return false;
-	}
-
 	SSD ssd;
 	std::string op_read = "R";
 	std::string op_write = "W";
+
+	if (op == op_write) {
+		if (isValidAddress(value) == false) {
+			std::cout << "InalidAddress:" << value << std::endl;
+			return false;
+		}
+	}
 
 	if (op == op_read) {
 		ssd.read(std::stoi(lba));
